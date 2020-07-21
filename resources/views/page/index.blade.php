d6a35bb5f19a540ec5e47c44a95538fead27c643
José Ramos <ramosneto1818@gmail.com>
2020-07-20 21:52:01 -0300

    Section de destaque

---
+++ resources/views/page/index.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.app')
+
+@section('content')
+
+    @include('session/destaque')
+    @include('session/personagem')
+
+@endsection
---
+++ resources/views/session/destaque.blade.php
@@ -0,0 +1,13 @@
+<section id="destaque">
+<div class="background d-flex justify-content-center">
+    <div class="card card-background text-center text-white">
+        <h4 id="personagem_nome">Personagem</h4>
+        <img id="personagem_card" 
+        src="https://pm1.narvii.com/6630/cbe2edf93a631b1892d463bd2bfd5cf2bc847432_hq.jpg" 
+        class="img-fluid game-card" alt="Personagem">
+        <div class="text-center mx-auto pb-4">
+        <p id="personagem-frase" class="card-frase">"Lorem ipsum dolor sit amet consectetur, adipisicing elit."</p>
+        </div>
+    </div>
+</div>
+</section>
